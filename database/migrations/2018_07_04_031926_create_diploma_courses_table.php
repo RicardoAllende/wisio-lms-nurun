56dbe66fb760644e2e8f770d63e8f3cf150f2b89
Ricardo Allende <ricardo.allende.p@gmail.com>
2018-07-03 23:35:49 -0500

    Se agrega tabla diploma_courses y pivote

---
+++ database/migrations/2018_07_04_031926_create_diploma_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiplomaCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diploma_courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('folio');
+            $table->uuid('hash');
+            $table->float('minimum_score', 10, 5)->default(8);
+            $table->integer('maximum_diplomas')->default(1);
+            $table->integer('course_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('code');
+            $table->enum('type', ['diploma', 'conamege']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diploma_courses');
+    }
+}
