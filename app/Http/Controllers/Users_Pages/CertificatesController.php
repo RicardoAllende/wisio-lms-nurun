12b5c9ccdd1493bb786767e21c57c6937060a7d4
Ricardo Allende <ricardo.allende@subitus.com>
2018-08-09 13:19:15 -0500

    Se recupera archivo eliminado

---
+++ app/Http/Controllers/Users_Pages/CertificatesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Users_Pages;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Ascription;
+use Illuminate\Support\Facades\Auth;
+
+class CertificatesController extends Controller
+{
+    public function list($ascription_slug){
+        $ascription = Ascription::whereSlug($ascription_slug)->first();
+        $user = Auth::user();
+        $certificates = $user->availableCertificates();
+        // dd($certificates);
+        $diplomas = $user->availableDiplomas();
+        // dd($diplomas);
+        return view('users_pages/certificates/list', compact('ascription', 'user', 'certificates', 'diplomas'));
+        // dd($certificates);
+        // return $ascription;
+    }
+}
