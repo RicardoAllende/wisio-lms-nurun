1749700cfa03975db3d2606652746d6ff3ddb1ed
Ricardo Allende <ricardo.allende@subitus.com>
2018-09-06 16:44:56 -0500

    Cambios en conexión con tincan y etiqueta de video para Internet Explorer

--- public/js/js_users_pages/tincanConnector.js
+++ public/js/js_users_pages/tincanConnector.js
@@ -2,7 +2,7 @@ var tincan = new TinCan (
     {
         recordStores: [
             {
-                endpoint: "http://dev.academia.nurun.com.mx:3000/data/xAPI",
+                endpoint: "https://dev.academia.nurun.com.mx/data/xAPI",
                 username: "a39b3e5b3ad82a2a842245cee1675b55d5df6d9a",
                 password: "81dc9a49b036f5b896afd97a7fcdb748849ca383",
                 allowFail: false
@@ -13,7 +13,7 @@ var tincan = new TinCan (
 
 var lrs = new TinCan.LRS(
     {
-        endpoint: "http://dev.academia.nurun.com.mx:3000/data/xAPI",
+        endpoint: "https://dev.academia.nurun.com.mx/data/xAPI",
         username: "a39b3e5b3ad82a2a842245cee1675b55d5df6d9a",
         password: "81dc9a49b036f5b896afd97a7fcdb748849ca383",
         allowFail: false
